02af3d3413b90521ca606001ab5d2c09201f124a
lbw <libowen1057@gmail.com>
2015-02-25 17:30:15 +0800

    modify detail page

---
+++ app/src/main/java/lbw/com/newsinfo/entity/NewsDetailEntity.java
@@ -0,0 +1,22 @@
+package lbw.com.newsinfo.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class NewsDetailEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String body;
+	public String image_source;
+	public String title;
+	public String image;
+	public String share_url;
+	public ArrayList<String> js;
+	public int type;
+	public String ga_prefix;
+	public long id;
+	public ArrayList<String> css;
+}
--- app/src/main/java/lbw/com/newsinfo/entity/NewsEntity.java
+++ app/src/main/java/lbw/com/newsinfo/entity/NewsEntity.java
@@ -14,21 +14,10 @@ public class NewsEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     public String title;
-    public String share_url;
     public String ga_prefix;
     public ArrayList<String> images;
     public int type;
     public long id;
-
-    public boolean subscribed;
-    public String theme_name;
-    public int theme_id;
-
-    //已读、未读标识
-    public boolean is_read = false;
-
-    //是否仅仅是个tag
-    public boolean isTag = false;
 
     @Override
     public String toString() {
